8c4bc33ebe4dcbfd9adedb8c4ea2f1c0b2b2f6f2
Adebogunabdulroheem <abdulroheemadebogun@gmail.com>
2022-03-24 15:31:43 +0100

    update

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { useRouter } from 'next/router'
 import styles from '../styles/Home.module.css'
-import Dashboard from './Dashboard'
-import Loader from './Loader'
+import Dashboard from '../components/Dashboard'
+import Loader from '../components/Loader'
 
 export default function Home() {
   const router = useRouter();
